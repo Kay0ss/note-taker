c84331181ebbea050a73731a2553bae77f0c01c8
Jack <85977030+Kay0ss@users.noreply.github.com>
2021-11-08 20:17:46 -0600

    finally no errors, time to see if it works

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,14 @@
 const path = require('path');
 const express = require('express');
-const note = require('./db/db.json');
+const data = require('./db/db.json');
 const fs = require("fs");
+const { v4: uuidv4} = require('uuid');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+let global = data;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -21,21 +24,28 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, './public/index.html'))
-);
+// app.get('*', (req, res) =>
+//     res.sendFile(path.join(__dirname, './public/index.html'))
+// );
 
-app.get('/api/notes', (req, res) =>
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
-);
+app.get('/api/notes', (req, res) => {
+    res.json(globalData);
+    console.log('line 32 ' + JSON.stringify(global));
+});
 
 app.post('/api/notes', (req, res) => {
-    if (req.body) {
-        readAndAppend(req.body, './db/db.json');
-        res.json(`Note added`);
-    }else{
-        res.error('Error while adding note');
-    }
+    let currData = req.body;
+    currData.id = uuidv4();
+
+    global.push(currData);
+
+    fs.writeFile('./db/db.json', JSON.stringify(global), (err) => {
+        err ? console.log(err) : console.log('Success!')
+    },
+        console.log(' line 38' + JSON.stringify(global))
+    );
+
+    res.json(global)
 });
 
 
