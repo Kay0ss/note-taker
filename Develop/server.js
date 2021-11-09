f3e17349a15725a6895531401e19c52316b7927b
Jack <85977030+Kay0ss@users.noreply.github.com>
2021-11-08 19:57:53 -0600

    server is running but have but where notes aren't saving

--- Develop/server.js
+++ Develop/server.js
@@ -1,18 +1,28 @@
 const path = require('path');
 const express = require('express');
-const api = require('./public/assets/js/index.js');
+const note = require('./db/db.json');
 const fs = require("fs");
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, './public/index.html'))
+    );
+
+
 app.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 app.get('/api/notes', (req, res) =>
@@ -27,3 +37,9 @@ app.post('/api/notes', (req, res) => {
         res.error('Error while adding note');
     }
 });
+
+
+
+app.listen(PORT, () =>
+console.log(`App listening on port 3001!`)
+);
