b4a4ece0fc2d69310e2e481aec7df0768e52e8dd
Jack <85977030+Kay0ss@users.noreply.github.com>
2021-11-10 20:47:36 -0600

    added delete to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const express = require("express");
-const data = require('./db/db.json');
+let data = require('./db/db.json');
 const fs = require("fs");
 const { v4: uuidv4} = require('uuid');
 
@@ -26,19 +26,19 @@ app.get('/notes', (req, res) =>
 );
 
 // app.get('*', (req, res) =>
-//     res.sendFile(path.join(__dirname, './public/index.html'))
+//     res.sendFile(path.join(__dirname, '/public/index.html'))
 // );
 
 app.get('/api/notes', (req, res) => {
     return res.json(data);
-    console.log('line 32 ' + JSON.stringify(global));
+    console.log('line 32 ' + JSON.stringify(data));
 });
 
 app.post('/api/notes', (req, res) => {
     let currData = req.body;
     currData.id = uuidv4();
 
-    global.push(currData);
+    data.push(currData);
 
     fs.writeFile('./db/db.json', JSON.stringify(data), (err) => {
         err ? console.error(err) : console.log('Success!')
@@ -48,5 +48,16 @@ app.post('/api/notes', (req, res) => {
 
     return res.json(data);
 });
+
+app.delete('/api/notes/:id', (req, res) => {
+    let toDeleteId = req.params.id;
+    data = data.filter(note => note.id != toDeleteId);
+
+    fs.writeFile('./db/db.json', JSON.stringify(data), (err) =>{
+        err ? console.error(err) : console.log('Twas a success!')
+    });
+
+    res.json(data);
+})
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
