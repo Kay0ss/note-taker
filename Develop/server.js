1d12f4bd7d353d18b797dd0c989c2918c5d0527d
Jack <85977030+Kay0ss@users.noreply.github.com>
2021-11-11 13:22:04 -0600

    commit for heroku

--- Develop/server.js
+++ Develop/server.js
@@ -25,9 +25,6 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// app.get('*', (req, res) =>
-//     res.sendFile(path.join(__dirname, '/public/index.html'))
-// );
 
 app.get('/api/notes', (req, res) => {
     return res.json(data);
