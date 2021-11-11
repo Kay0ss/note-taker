91560baec342e991355c444d89a3e2b3320c2f7a
Jack <85977030+Kay0ss@users.noreply.github.com>
2021-11-11 14:40:20 -0600

    hopefully last heroku push

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -3,7 +3,7 @@ let noteText;
 let saveNoteBtn;
 let newNoteBtn;
 let noteList;
-// test
+// test tt
 
 if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ let data = require('./db/db.json');
 const fs = require("fs");
 const { v4: uuidv4} = require('uuid');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
