a576abb5cae7f32cbd690ed63a0503ad394c7346
Jack <85977030+Kay0ss@users.noreply.github.com>
2021-11-13 14:38:09 -0600

    finalized readme, and did some final formattign to my code

--- README.md
+++ README.md
@@ -1,3 +1,66 @@
-# note-taker
+# Note-Taker
 
-testing for 
+# Description
+## A simple application used for taking and storing notes through your browser.
+
+# Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Screenshot](#screenshot)
+- [Active Link](#active-link)
+- [Credits](#credits)
+- [License](#license)
+- [Questions](#questions)
+
+
+
+# Installation
+## You can start the app by running `node server.js` in the develop folders terminal.
+
+
+# Usage
+## Once you have started the server, go to "localhost:3000" in your browser to use the application.
+
+# Screenshot
+![image](./Assets/note-taker.png)
+
+# Active Link
+https://note-taker-jrt.herokuapp.com/
+
+
+# Credits
+## Jack Theisen - https://github.com/Kay0ss
+
+
+# License
+## MIT License
+
+Copyright (c)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
+# Questions
+## https://github.com/Kay0ss
+## You can email me at anytime.
+
+
+    
+    
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const path = require("path");
 const express = require("express");
-let data = require('./db/db.json');
+let data = require("./db/db.json");
 const fs = require("fs");
-const { v4: uuidv4} = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,48 +13,50 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
 // GET Route for feedback page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-
-app.get('/api/notes', (req, res) => {
-    return res.json(data);
-    console.log('line 32 ' + JSON.stringify(data));
+app.get("/api/notes", (req, res) => {
+  return res.json(data);
+  console.log("line 32 " + JSON.stringify(data));
 });
 
-app.post('/api/notes', (req, res) => {
-    let currData = req.body;
-    currData.id = uuidv4();
+app.post("/api/notes", (req, res) => {
+  let currData = req.body;
+  currData.id = uuidv4();
 
-    data.push(currData);
+  data.push(currData);
 
-    fs.writeFile('./db/db.json', JSON.stringify(data), (err) => {
-        err ? console.error(err) : console.log('Success!')
+  fs.writeFile(
+    "./db/db.json",
+    JSON.stringify(data),
+    (err) => {
+      err ? console.error(err) : console.log("Success!");
     },
-        console.log('line 38' + JSON.stringify(data))
-    );
+    console.log("line 38" + JSON.stringify(data))
+  );
 
-    return res.json(data);
+  return res.json(data);
 });
 
-app.delete('/api/notes/:id', (req, res) => {
-    let toDeleteId = req.params.id;
-    data = data.filter(note => note.id != toDeleteId);
+app.delete("/api/notes/:id", (req, res) => {
+  let toDeleteId = req.params.id;
+  data = data.filter((note) => note.id != toDeleteId);
 
-    fs.writeFile('./db/db.json', JSON.stringify(data), (err) =>{
-        err ? console.error(err) : console.log('Twas a success!')
-    });
+  fs.writeFile("./db/db.json", JSON.stringify(data), (err) => {
+    err ? console.error(err) : console.log("Twas a success!");
+  });
 
-    res.json(data);
-})
+  res.json(data);
+});
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
